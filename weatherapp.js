cd88ab9bc591b9fcbbb0a3ed0db609529bb32434
yqureishy <yqureishy1993@gmail.com>
2021-03-10 15:17:24 -0600

    added date to C2, added F, removed 'time', 'temp'

--- weatherapp.js
+++ weatherapp.js
@@ -5,6 +5,7 @@ const tomorrowsWeatherUL = document.getElementById("tomorrowsWeatherUL");
 // const latLongUL = document.getElementById("latLongUL")
 const stateNameTextBox = document.getElementById("stateNameTextBox")
 const dateTodayHeader = document.getElementById("dateTodayHeader")
+const dateTomorrowHeader = document.getElementById("dateTomorrowHeader")
 
 
 
@@ -32,14 +33,18 @@ searchButton.addEventListener('click', function(){
             let displayTemp = ` 
                             <div class="weatherInfo">
 
-                            <div>Time: ${new Date(Date.parse(allWeather.timestamp_local)).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} |
-                            Feels Like Temp: ${allWeather.app_temp}<span>&#176;</span></div>
+                            <div>${new Date(Date.parse(allWeather.timestamp_local)).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} |
+                            Feels Like: ${parseInt(allWeather.app_temp)}<span>&#176;</span>F</div>
                             <div><img id="clothesDisplay"> ${clothingApparel(allWeather.app_temp)}</img></div>
                             <br>
                             <br>
                             </div>`
 
             // todaysWeatherUL.insertAdjacentHTML('beforeend', displayTemp) 
+
+            dateTodayHeader.innerHTML = `<h4> Today's Forecast </h4><p style="font-size: 15px">${new Date(Date.parse(result.data[0].timestamp_local)).toDateString().split(' ').slice(0,3).join(' ')}</p>`
+
+            dateTomorrowHeader.innerHTML = `<h4> Tomorrow's Forecast </h4><p style="font-size: 15px">${new Date(Date.parse(result.data[24].timestamp_local)).toDateString().split(' ').slice(0,3).join(' ')}</p>`
 
             let apiTime = new Date(Date.parse(result.data[index].timestamp_local)).toLocaleDateString()
             // let apiTime = allWeather.timestamp_local
@@ -60,46 +65,48 @@ searchButton.addEventListener('click', function(){
         }
             
         })
-        dateTodayHeader.innerHTML = `<h4> Today's Forecast: </h4><p style="font-size: 15px">${new Date(Date.parse(result.data[0].timestamp_local)).toDateString().split(' ').slice(0,3).join(' ')}</p>`
-    })
+})
+    
 
 function clothingApparel(temp) {
     if (temp > 80){
         tank = "<img src='clothes/008-tank-top.png' width= 64 height= 64></img>";
         return tank
     }
-    else if (temp <= 80 && temp > 70 ){
+    else if (temp <= 80 && temp > 69.9 ){
         tshirt = "<img src='clothes/007-tshirt.png'width= 64 height= 64></img>";
         return tshirt
     }
-    else if (temp <= 70 && temp > 60 ){
+    else if (temp <= 70 && temp > 59.9 ){
         longSleeveShirt ="<img src= 'clothes/006-longsleeve.png'width= 64 height= 64></img>";
         tshirt = "<img src='clothes/007-tshirt.png'width= 64 height= 64></img>";
         return `${tshirt} + ${longSleeveShirt}`
     }
-    else if (temp <= 60 && temp > 50 ){
+    else if (temp <= 60 && temp > 49.9 ){
         longSleeveShirt="<img src= 'clothes/006-longsleeve.png' width= 64 height= 64></img>";
         hoody="<img src= 'clothes/005-hoodie.png' width= 64 height= 64></img>";
         return `${longSleeveShirt} + ${hoody}`
     }
-    else if (temp <= 50 && temp > 40 ){
+    else if (temp <= 50 && temp > 39.9 ){
         longSleeveShirt="<img src= 'clothes/006-longsleeve.png' width= 64 height= 64></img>";
         hoody="<img src= 'clothes/005-hoodie.png'width= 64 height= 64></img>";
         jacket="<img src= 'clothes/004-jacket-1.png'width= 64 height= 64></img>";
         return `${longSleeveShirt} + ${hoody} + ${jacket}`
     }
-    else if (temp <= 40 && temp > 30 ){
+    else if (temp <= 40 && temp > 29.9 ){
         coat = "<img src='clothes/003-coat.png'width= 64 height= 64></img>";
         jacket = "<img src= 'clothes/004-jacket-1.png'width= 64 height= 64></img>";
 
         return `${jacket} + ${coat}`
     }
-    else if (temp <= 30){
+    else if (temp <= 29.9){
         coat = "<img src= 'clothes/003-coat.png'width= 64 height= 64></img>";
         hat = "<img src= 'clothes/001-winter-hat.png'width= 64 height= 64></img> ";
         scarf = "<img src= 'clothes/002-scarf.png'width= 64 height= 64></img>";
         return `${coat} + ${hat} + ${scarf}`
     }}
+
+
 
 
 // WEATHER BY LAT/LONG
